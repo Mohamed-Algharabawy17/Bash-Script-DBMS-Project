a6c82a7cc22dcd03e7c9495b94029722a5d23772
mohamedtorkey29 <mohamedtorkey132@gmail.com>
2023-12-22 01:56:05 +0200

    we added update function and enhance in code

--- DBMS.sh
+++ DBMS.sh
@@ -3,6 +3,9 @@
 #directry to store all database files 
 mkdir Main 2>.error
 
+
+#*************************Table_Menu Opetions on Tables in Database**************************************
+
 function Table_Menu
 {
 echo "***************************"
@@ -23,7 +26,7 @@ echo "***************************"
     case $choice in
         1) Creat_Table
             ;;
-        2) ls /home/mohamedtorkey/Main; Table_Menu
+        2) ls $PWD/Main; Table_Menu
             ;;
         3) Drop_Table
             ;;
@@ -33,9 +36,9 @@ echo "***************************"
             ;;
        6) echo "Delete From Table"
             ;;
-       7) echo "update table"
+       7) Table_update
            ;;
-       8) break 2>> /home/mohamedtorkey/.error
+       8) break 2>> $PWD/.errors
             ;;
        *)echo "Invalid choice"
             ;;
@@ -43,86 +46,182 @@ echo "***************************"
 
 }
 
+#************************Creat Table Functions**********************************
 
 function Creat_Table
 {
-  echo -e "Enter Your Table name:\c"
-   read f_name 
-   if [ -f "/home/mohamedtorkey/Main/$f_name" ]
+   read -p "Enter Your Table name:" Table_name
+ 
+   if [ -f "$PWD/Main/$Table_name" ]
     then
-   echo "$f_name is already created ,please Enter the anther file" ; Creat_Table
-   else
-   touch "/home/mohamedtorkey/Main/$f_name"
-    echo -e "$f_name Table is Created succesfuly(:\n"
-   fi
-    typeset -i NCols
+         echo "$Table_name is already created ,please Enter the anther Name Table " ; Creat_Table
+
+       else
+
+          touch "$Table_name"
+          touch "$Table_name"
+
+          echo "$Table_name Table is Created succesfuly(:"
+  fi
+
+    typeset -i NC
     typeset -i i
-    echo -e "Enter the number of columns:\c"
-     read NUCols 
+    typeset -i j	
+
+     j=1
+    read -p "Enter the number of columns:" N_columns
      i=1
-     
-           Data="" 
-      until [ $i -gt $NUCols ]
-       do
-      echo -e "Enter the name of column $i:\c"
-      read name_col
-      opn="("
-      sep=","
-      cls=")"
+     Data="" 
+	   
+    until [ $i -gt $N_columns ]
+     do
+         read -p "Enter the name of column.$i:" Name_column
       
-         Data+=$name_col
+         Data+="$Name_column"
+	 Info="$Name_column"
 	
-      echo -e "Type of Column $name_col:\n"
-       select ty_col in "int" "str"
-         do
-           case $ty_col in
-           int ) ty_int="int";break;;
-           str ) ty_str="str";break;;
+         echo "Type of Column $Name_column:"
+
+         select type_col in "int" "str"
+           do
+           case $type_col in
+
+           "int" )type_int="int"; break;;
+           "str" )type_str="str";break;;
            * ) echo "Wrong Choice" ;;
            esac
 	 done    
-	   Data+="($ty_col"
+
+	Info+="|$type_col"
+
         
-	
-       if [ $ty_col = "int" ]
-       then
-	       echo -e "Do You make $name_col is Primary key?(Y/N):\c"
-	        read chpk
-
-		if [ $chpk = "Y" ]
-		then
-			Data+=",PK)|"
-			echo "DONE"
-		else
-			Data+=")|"
-			echo "Fail"
-		fi	
-                 
+          	
+            
+	  if [ $type_col == "int" ]
+	  then
+            if [[ $j == 1 ]]
+	    then
 	        
-	else
-		Data+=")|"
-      fi
-      
-      i+=1
-      echo $Data > "/home/mohamedtorkey/Main/$f_name"
-      done
+	       read -p "Do You make $Name_column is Primary key?[y/n]:" check
+	        
 
-       
+		if [ $check = "y" ]
+		then
+			Info+="|PK|"
+		      j=2
+		        echo "DONE"	
+			
+		fi	
+	    fi
+
+      fi
+       Data+="|"
+      
+      echo $Data > "$PWD/Main/$Table_name"
+      echo $Info >> "$PWD/Main/.$Table_name"
+      
+
+
+      i+=1
+      done
+    echo "The Data Compeleted successfully"  
+ Table_Menu       
  
 	        
 }
                     
             
-           
+#**************************Drop Table funciotn**************************************************************
+
 function Drop_Table {
   echo -e "Enter Name of Table:\c"
-  read tab_name
-  rm  "/home/mohamedtorkey/Main/$tab_name" 2>.error
-  if [ $? -eq 0 ]
-  then
-    echo "the Table Dropped Successfully(:"
+   read tab_name
 
-  fi
+  if [ -f "$PWD/Main/$tab_name" ]
+  then
+
+   echo -e "Your sure to drop the $table_name table\n"
+
+ select opt in "yes" "no"
+   do
+  case $opt in 
+   "yes")    
+    rm "$PWD/Main/$tab_name" 2>.error
+    
+    if [ $? -eq 0 ]
+    then
+    echo "the Table Dropped Successfully(:"
+    fi 
+    break 2>.error ;;
+
+   "no") break 2>.error ;;
+  esac
+ done
+else
+   echo "the $tab_name doesn't exit"	
+   
+  fi 
+
   Table_Menu
 }
+
+#***************************Update Table function*************************************************************
+
+
+function Table_update {
+
+  read -p  "Enter The Table Name:" Table_name
+ 
+    if [ -f $PWD/Main/$Table_name ]
+    then
+  
+     read -p "Enter Condition Column name:" Field_name
+      res1=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$Field_name'") print i}}}' $PWD/Main/$Table_name )
+
+   
+     if [[ $res1 == "" ]]
+     then
+	     
+            echo "Field name Doesn't Exit"
+            Table_Menu
+     else
+            read -p "Enter Value of Condition Column:" Value
+     
+
+        res2=$(cut -d'|' -f$res1 "$PWD/Main/$Table_name" | grep  "^$Value$")
+	
+        if [[ $res2 == "" ]]
+	then
+		echo "the Value of Column Doesn't Exit"
+		Table_Menu
+	else	
+         read -p "Enter Field Name: " Field
+      
+      res3=$(awk  'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$Field'") print i}}}' $PWD/Main/$Table_name)
+      if [[ $res3 == "" ]]
+      then
+        echo "Field name Doesn't Exit"
+        Table_Menu
+      else
+        read -p "Enter new Value of Field:" Value_Field
+
+	 Number_Record=$(cut -d'|' -f$res1 "$PWD/Main/$Table_name" | grep -n "^$Value$"| cut -d':' -f1)
+
+        Current_data=$(awk  -F'|' '{if(NR=='$Number_Record'){for(i=1;i<=NF;i++){if(i=='$res3') print $i}}}' $PWD/Main/$Table_name 2>>.errors)
+
+        sed -i ''$Number_Record's/'$Current_data'/'$Value_Field'/g' "$PWD/Main/$Table_name" 2>>.errors
+        echo "Row Updated Successfully"
+        Table_Menu
+      fi
+    fi
+  fi
+else
+	echo "Table Doesn't Exit"
+	Table_update
+    fi
+}
+
+
+
+
 Table_Menu
